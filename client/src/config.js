f609b2b1b6d104510c8c67c86990a6cf04afb697
Pranjal <pranjalgosavi2507@gmail.com>
2026-03-08 16:04:39 +0530

    feat: Add API URL configuration that differentiates between development and production environments.

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.DEV
     ? "http://localhost:3000"
-    : "https://pranjal-s56-capstone-expense-tracker.onrender.com";
+    : "https://ifinance-expense-tracker.onrender.com";
 
 export default API_URL;
